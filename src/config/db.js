ff844b9b561b96c8d8120422338fdde6dfb811bb
Abdur Rahman <abdulrehmanmazhar02@gmail.com>
2024-09-20 01:18:51 +0500

    client added

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
-    const database = process.env.MONGO_URI;
-    console.log(database)
+    const database = process.env.MONGODB_URI;
     try {
         const connection = await mongoose.connect(`${database}/${DB_NAME}`);
         console.log(`DB is connected on host ${connection.connection.host}`)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-
-
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import connectDB from "./config/db.js";
 
-
+// Environment Variable configuration
 dotenv.config();
+
 
 const app = express();
 
+
+// Middlewares
+app.use(cors());
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+// Database Connection 
 connectDB();
 
-
+// Routes
 app.get('/', () => console.log(`working at port ${process.env.PORT}`));
+
 
 app.listen(process.env.PORT)
